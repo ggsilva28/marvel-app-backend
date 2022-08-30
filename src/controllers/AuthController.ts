3f7e83b9dcc15f3b9c54dbd8027cd094e5dc3a0a
Gabriel Gonçalves <gagsilva28@gmail.com>
2022-08-29 21:17:13 -0300

    Fixing

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express"
 import { AuthService } from "../services/AuthService"
 
+
 class AuthController {
 
     async login(request: Request, response: Response) {
